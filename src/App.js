c72cb4236e171e97d0a92256686e3f33bc4713fc
faris <mrfaristp@gmail.com>
2023-07-09 12:13:43 +0530

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> hello react.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="app-header">
+      <span>Home</span>
+      <span>usage</span>
+      <span>settting</span>
+      <span>Log out</span>
     </div>
+    <div class="app-body">
+      <div class="appList">
+        <div class="listItem">
+          <div class="listTittle">
+            <h3>my tittle</h3>
+          </div>
+          <div class="listDescription">
+            just for ddemmey
+          </div>
+          <div class="listLabel">
+            <span>Label 1</span>
+            <span>Label 2</span>
+            <span>Label 3</span>
+          </div>
+          <div class="listLabel">
+            <span>Label 1</span>
+            <span>Label 2</span>
+            <span>Label 3</span>
+          </div>
+          <div class="listLabel">
+            <span>Label 1</span>
+            <span>Label 2</span>
+            <span>Label 3</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="app-footer">
+      copyrighted by markdown
+    </div>
+  </div>
   );
 }
 
